5e01fbf5b0aa15c4d4fa43eee659e29f0214d0fd
AIYEVBEKPEN DIVNE OTASOWIE <144267777+bobbytech1@users.noreply.github.com>
2024-12-17 02:03:23 +0100

    Delete src directory

--- src/pages/Events.tsx
+++
@@ -1,9 +0,0 @@
-const Events = () => {
-    return ( 
-        <>
-            <h1>Hello this is Events Page</h1>
-        </>
-     );
-}
- 
-export default Events;
--- src/pages/Ministries.tsx
+++
@@ -1,9 +0,0 @@
-const Ministries = () => {
-    return ( 
-        <>
-            <h1>Hello this is Ministries Page</h1>
-        </>
-     );
-}
- 
-export default Ministries;
