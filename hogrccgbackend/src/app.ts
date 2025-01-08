6db0917bb4f66adda0011ad8596c1b3cf287a762
bobbytech1 <aiyevbekpendivine@gmail.com>
2025-01-08 16:40:29 +0100

    Changed the cors for testing purpose only

--- hogrccgbackend/src/app.ts
+++ hogrccgbackend/src/app.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: '*', // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies to be sent across origins
--- hogrccgbackend/src/db/knex.ts
+++ hogrccgbackend/src/db/knex.ts
@@ -2,7 +2,7 @@ import knex from 'knex';
 import knexConfig from '../config/knexConfig';
 
 // Debugging: Log Knex configuration to verify connection details
-console.log('Knex configuration:', knexConfig);
+
 
 const db = knex(knexConfig);
 
