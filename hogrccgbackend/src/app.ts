ef043b1b68b77a99a8bc7f701325abf5d7eef4cf
bobbytech1 <aiyevbekpendivine@gmail.com>
2025-01-19 08:43:33 +0100

    Made some modifications to cors policy

--- hogrccgbackend/src/app.ts
+++ hogrccgbackend/src/app.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 // CORS configuration
 const corsOptions = {
-  origin: '*', // Replace with your frontend URL
+  origin: ['http://localhost:5173', 'https://hogrccgpreview.onrender.com'], // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies to be sent across origins
